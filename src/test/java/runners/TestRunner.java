a72ce946652d69651c7133b2f7038e770b013c6d
Ebubekir <esahin3@icloud.com>
2021-07-12 12:49:25 +0300

    repair

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -13,7 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = "stepdefinitions",
-        dryRun = false
+        dryRun = false,
+        tags = "@"
 )
 
 public class TestRunner {
