5d099835c90d394a259ba99bdbd5cb779a38665b
Ebubekir <esahin3@icloud.com>
2021-07-13 23:05:42 +0300

    repair

--- src/test/java/stepdefinitions/GoogleTitleStepDefs.java
+++ src/test/java/stepdefinitions/GoogleTitleStepDefs.java
@@ -36,7 +36,7 @@ public class GoogleTitleStepDefs {
 
     @Then("User validates the url" )
     public void userValidatesTheUrl() {
-        Assert.assertTrue(url.contains("google"));
+        Assert.assertTrue(url.contains("aws"));
         System.out.println("URL Test PASSED");
     }
 }
