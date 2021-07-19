cdb85e0b4e4a1f12a57f9a520f849ce3ff527973
Ebubekir <esahin3@icloud.com>
2021-07-19 17:25:31 +0300

    repair

--- src/test/java/stepdefinitions/AmazonSeleniumGridStepDefs.java
+++ src/test/java/stepdefinitions/AmazonSeleniumGridStepDefs.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+@SuppressWarnings("ALL")
 public class AmazonSeleniumGridStepDefs {
 
     // Bir WebDrivero object'i uret
